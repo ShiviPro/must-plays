43f8e7f84c0dcca170e1d0027a5873e8a49efeb0
Shivam Tewari <www.shivamtewari@gmail.com>
2021-08-06 12:01:10 +0530

    feat: added gameData; style: added genres to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import "./App.css";
+import mustPlayGames from "./data/gameData";
 
 function App() {
+  let gameGenres = Object.keys(mustPlayGames);
+
+  for (let genreIndex = 0; genreIndex < gameGenres.length; genreIndex++) {
+    while (gameGenres[genreIndex].includes("_")) {
+      gameGenres[genreIndex] = gameGenres[genreIndex].replace("_", " ");
+    }
+  }
+
+  let gameGenreDivs = gameGenres.map((gameGenre, index) => (
+    <div key={index} className="game-genre">
+      {gameGenre}
+    </div>
+  ));
+
   return (
     <div className="App">
-      <h1>Must Plays</h1>
+      <h1 className="brand-name">Must Plays</h1>
+      <p className="hero-text">
+        Check out my list of must play games/franchises. Select a genre to begin
+        with -
+      </p>
+      <p className="genre-heading">Genres: </p>
+      <div className="game-genres">{gameGenreDivs}</div>
     </div>
   );
 }
