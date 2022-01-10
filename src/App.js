1b89038d698328a36dbfec6526bcaee5f246b09c
Shivam Tewari <www.shivamtewari@gmail.com>
2022-01-10 18:29:15 +0530

    refactor: non-var to ES6 const; trim ES6 fat

--- src/App.js
+++ src/App.js
@@ -2,59 +2,54 @@ import { useState } from "react";
 import "./App.css";
 import mustPlayGames from "./data/gameData";
 
-function App() {
-  let gameGenres = Object.keys(mustPlayGames);
+const App = () => {
+  const gameGenres = Object.keys(mustPlayGames);
 
-  for (let genreIndex = 0; genreIndex < gameGenres.length; genreIndex++) {
-    while (gameGenres[genreIndex].includes("_")) {
+  for (let genreIndex = 0; genreIndex < gameGenres.length; genreIndex++)
+    while (gameGenres[genreIndex].includes("_"))
       gameGenres[genreIndex] = gameGenres[genreIndex].replace("_", " ");
-    }
-  }
 
-  let [userSelection, setUserSelection] = useState("None");
-  let [isGenreSelected, setIsGenreSelected] = useState(false);
+  const [userSelection, setUserSelection] = useState("None");
+  const [isGenreSelected, setIsGenreSelected] = useState(false);
 
-  const selectGenre = (event) => {
+  const selectGenre = event => {
     setIsGenreSelected(true);
-    userSelection = event.target.innerText;
-    while (userSelection.includes(" ")) {
-      userSelection = userSelection.replace(" ", "_");
-    }
-    setUserSelection(userSelection);
+    let currUserSelection = event.target.innerText;
+    while (currUserSelection.includes(" ")) 
+      currUserSelection = currUserSelection.replace(" ", "_");
+    setUserSelection(currUserSelection);
   };
 
-  let gameGenreDivs = gameGenres.map((gameGenre, index) => (
+  const gameGenreDivs = gameGenres.map((gameGenre, index) => 
     <div key={index} className="game-genre" onClick={selectGenre}>
       {gameGenre}
     </div>
-  ));
+  );
 
-  function Games(props) {
-    if (isGenreSelected) {
-      let genre = props.genre;
-      let games = mustPlayGames[genre];
-      let gameInfoDivs = games.map((game) => {
-        let platforms = game.platforms;
-        let platformDivs = platforms.map((platform, index) => (
-          <span key={index} className="game-platform">
-            {platform}
-          </span>
-        ));
-        return (
-          <div key="index" className="game-info">
-            <h2 className="game-name">{game.name}</h2>
-            <p className="game-platforms">{platformDivs}</p>
-            <h3 className="game-release-date">
-              Released on {game.originalReleaseDate}
-            </h3>
-            <h2>My Rating: {game.myRating}</h2>
-            <p className="game-intro">{game.introduction}</p>
-          </div>
-        );
-      });
-      return gameInfoDivs;
-    }
-    return null;
+  const Games = props => {
+    if (!isGenreSelected) return null;
+    const genre = props.genre;
+    const games = mustPlayGames[genre];
+    const gameInfoDivs = games.map(game => {
+      const platforms = game.platforms;
+      const platformDivs = platforms.map((platform, index) =>
+        <span key={index} className="game-platform">
+          {platform}
+        </span>
+      );
+      return (
+        <div key="index" className="game-info">
+          <h2 className="game-name">{game.name}</h2>
+          <p className="game-platforms">{platformDivs}</p>
+          <h3 className="game-release-date">
+            Released on {game.originalReleaseDate}
+          </h3>
+          <h2>My Rating: {game.myRating}</h2>
+          <p className="game-intro">{game.introduction}</p>
+        </div>
+      );
+    });
+    return gameInfoDivs;
   }
 
   return (
